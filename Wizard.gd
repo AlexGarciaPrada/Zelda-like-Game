ae30847787bd635c21e4597b7983c04bd822dc73
AlexGarciaPrada <a.garciap.2022@alumnos.urjc.es>
2024-06-01 19:43:55 +0200

    huh

--- Wizard.gd
+++ Wizard.gd
@@ -3,22 +3,20 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+@onready var animation = $AnimatedSprite2D
 func _physics_process(delta):
-	
-	# Handle Jump.
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var directionx = Input.get_axis("ui_left", "ui_right")
 	var directiony = Input.get_axis("ui_up","ui_down")
 	if directionx or directiony:
+		animation.play("walk")
 		velocity.x = directionx * SPEED
 		velocity.y = directiony *SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y,0,SPEED)
+		animation.play("idle")
 #	_run()
+	
 	move_and_slide()
 func _run():
 	if Input.is_key_pressed(KEY_CTRL) :
