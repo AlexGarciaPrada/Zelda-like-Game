054002ad2c8e0886f013309abf0013b7d658d40e
AlexGarciaPrada <a.garciap.2022@alumnos.urjc.es>
2024-06-02 23:05:52 +0200

    Ahora ya se pueden matar los monstruos

--- Enemies/enemy_proof.gd
+++ Enemies/enemy_proof.gd
@@ -1,15 +1,21 @@
 extends CharacterBody2D
 
-@onready var player = %Wizard
-var speed = 300
+@onready var player = $"../Wizard"
+var speed = 200
 var range = 400
 var life = 1
 
 
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("Weapon"):
+		life -= 1
 
 func _physics_process(delta):
 	if life < 1:
 		queue_free()
-	if sqrt((position.x-player.position.x)*(position.x-player.position.x)+(position.y-player.position.y)*(position.y-player.position.y)) <= range:
+	
+	var distance_to_player = position.distance_to(player.position)
+	if distance_to_player <= range:
 		velocity = position.direction_to(player.position) * speed
 		move_and_slide()
+
