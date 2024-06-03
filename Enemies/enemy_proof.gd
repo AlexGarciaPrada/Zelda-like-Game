8c0cb3ef11d6b2c09cf3c91ca8bc8f29da8ec2e9
AlexGarciaPrada <a.garciap.2022@alumnos.urjc.es>
2024-06-03 10:18:08 +0200

    Arreglado un error relacionado con las armas

--- Enemies/enemy_proof.gd
+++ Enemies/enemy_proof.gd
@@ -4,13 +4,18 @@ extends CharacterBody2D
 var speed = 200
 var range = 400
 var life = 1
-
+var weapons_in_area = []
 
 func _on_area_2d_area_entered(area):
 	if area.is_in_group("Weapon"):
-		life -= 1
+		weapons_in_area.append(area)
 
 func _physics_process(delta):
+	for weapon in weapons_in_area:
+		if weapon.is_visible_in_tree():
+			life -= 1
+			weapons_in_area.erase(weapon)
+			break
 	if life < 1:
 		queue_free()
 	
@@ -19,3 +24,7 @@ func _physics_process(delta):
 		velocity = position.direction_to(player.position) * speed
 		move_and_slide()
 
+
+func _on_area_2d_area_exited(area):
+	if area.is_in_group("Weapon"):
+		weapons_in_area.erase(area)
