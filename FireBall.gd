fa3f07fc27eab609856e47ade45965ea740c9421
WillyWonkaXD10 <guillegarciaprada@gmail.com>
2024-06-05 17:21:03 +0200

    explosion fireball

--- FireBall.gd
+++ FireBall.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 600
 @onready var animation =$AnimatedSprite2D
+var collision = false
 func _ready():
 	animation.play("fireball")
 func _physics_process(delta):
@@ -10,3 +11,17 @@ func _physics_process(delta):
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("Environment") or area.is_in_group("Enemy"):
+		animation.scale = animation.scale * 0.6
+		animation.play("explosion")
+		velocity = Vector2(0,0)
+		collision=true
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if collision:
+		queue_free()
