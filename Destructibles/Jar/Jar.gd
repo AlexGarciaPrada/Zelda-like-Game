1f63a29833f99b8bf6ba2d262a7a501e5df596c1
AlexGarciaPrada <a.garciap.2022@alumnos.urjc.es>
2024-07-02 17:38:09 +0200

    Jarron

---
+++ Destructibles/Jar/Jar.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@onready var animation = $AnimatedSprite2D
+@onready var collision = $CollisionShape2D
+var weapons_in_area = []
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation.play("idle")
+func _physics_process(delta):
+	for weapon in weapons_in_area:
+		if weapon.is_visible_in_tree():
+			animation.play("roto")
+			if collision != null:
+				collision.queue_free()
+
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("Weapon") :
+		weapons_in_area.append(area)
+
+
+func _on_area_2d_area_exited(area):
+	weapons_in_area.erase(area)
