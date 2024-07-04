31d37156358ece709c078916a7fa36c692757787
AlexGarciaPrada <a.garciap.2022@alumnos.urjc.es>
2024-07-04 17:06:13 +0200

    jarrón arreglado

--- Destructibles/Jar/Jar.gd
+++ Destructibles/Jar/Jar.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var animation = $AnimatedSprite2D
 @onready var collision = $CollisionShape2D
+@onready var area = $Area2D
 var weapons_in_area = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +13,8 @@ func _physics_process(delta):
 			animation.play("roto")
 			if collision != null:
 				collision.queue_free()
-
+			if area != null:
+				area.queue_free()
 
 
 func _on_area_2d_area_entered(area):
