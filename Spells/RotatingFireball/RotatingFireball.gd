43a27070c5bda615de9b5045a6b391dab4a59cec
AlexGarciaPrada <a.garciap.2022@alumnos.urjc.es>
2024-07-08 20:19:27 +0200

    .

--- Spells/RotatingFireball/RotatingFireball.gd
+++ Spells/RotatingFireball/RotatingFireball.gd
@@ -4,7 +4,9 @@ extends CharacterBody2D
 var center = Node2D
 @export var orbit_radius: float = 50.0
 @export var orbit_speed: float = 3.0  # Radianes por segundo
+@onready var area = $Area2D
 var angle: float = 0.0
+var explosion = false
 
 func _ready():
 	animation.play("orbit")
@@ -15,7 +17,7 @@ func _ready():
 		update_position()
 
 func _process(delta):
-	if center != null:
+	if center != null && !explosion:
 		angle += orbit_speed * delta
 		update_position()
 		update_rotation()
@@ -33,4 +35,13 @@ func update_rotation():
 
 func _on_area_2d_area_entered(area):
 	if area.is_in_group("Enemy"):
+		animation.scale = Vector2(0.7,0.7)
+		velocity = Vector2(0,0)
+		animation.play("explosion")
+		explosion = true
+		area.queue_free()
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if explosion:
 		queue_free()
