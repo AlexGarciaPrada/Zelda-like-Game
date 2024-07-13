c37042f1ac9ce4a5ed1faded0afa898002be325b
AlexGarciaPrada <a.garciap.2022@alumnos.urjc.es>
2024-07-13 02:21:28 +0200

    Aun no me convence

--- HUD/IconsContainerV2/IconsContainerV2.gd
+++ HUD/IconsContainerV2/IconsContainerV2.gd
@@ -1,12 +1,30 @@
 extends StaticBody2D
 
 @export var number_label = ""
-
+var icon_scene = preload("res://HUD/IconsV2/IconsV2.tscn")
+@export var texture_path : String
+var mouse_in = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Label.text = number_label
-
+	if texture_path != null:
+		$TextureRect.texture = load(texture_path)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if mouse_in:
+		if $TextureRect.texture != null:
+			var icon = icon_scene.instantiate()
+			icon.this_texture = $TextureRect.texture
+			self.add_child(icon)
+			mouse_in = false
+
+		
+
+
+func _on_area_2d_mouse_entered():
+	mouse_in = true
+	
+
+func _on_area_2d_mouse_exited():
+	mouse_in = false
--- HUD/IconsV2/IconsV2.gd
+++ HUD/IconsV2/IconsV2.gd
@@ -7,11 +7,16 @@ var not_done = false
 var offset : Vector2
 var initialPos: Vector2
 var previous_texture : CompressedTexture2D
-# Called when the node enters the scene tree for the first time.
+@onready var sprite = $Sprite2D
+@export var this_texture :CompressedTexture2D
+func _ready():
+	if this_texture != null:
+		sprite.texture = this_texture
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if draggable:
+		visible = true
 		if Input.is_action_just_pressed("click"):
 			initialPos = global_position
 			offset = get_global_mouse_position()-global_position
@@ -19,25 +24,26 @@ func _process(delta):
 		if Input.is_action_pressed("click"):
 			offset = get_global_mouse_position()-global_position
 			global_position = get_global_mouse_position()
+			self.scale = Vector2 (1.5,1.5)
 		elif Input.is_action_just_released("click"):
 			Singleton.is_dragging = false
 			var tween = get_tree().create_tween()
 			if is_inside_droppable:
 				tween.tween_property(self,"position",body_ref.position,0.2).set_ease(Tween.EASE_OUT)
 				body_ref.get_child(2).texture = self.get_child(0).texture
+				queue_free()
 	
 			else:
 				tween.tween_property(self,"global_position",initialPos,0.2).set_ease(Tween.EASE_OUT)
+				queue_free()
 func _on_area_2d_mouse_entered():
 	if !Singleton.is_dragging:
 		draggable = true
-		scale = Vector2(1.05,1.05)
 
 
 func _on_area_2d_mouse_exited():
 	if !Singleton.is_dragging:
 		draggable = false
-		scale = Vector2(1,1)
 
 
 
@@ -45,8 +51,10 @@ func _on_area_2d_body_entered(body:StaticBody2D):
 	if body.is_in_group("droppable"):
 		is_inside_droppable = true
 		body_ref = body
+		body.modulate = Color(Color.RED)
 
 
 func _on_area_2d_body_exited(body):
 	if body.is_in_group("droppable"):
 		is_inside_droppable = false
+		body.modulate = Color(Color.WHITE)
