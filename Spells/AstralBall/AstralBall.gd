431b5b72a74dde39bbf48b86ff6b23a01e312f9a
AlexGarciaPrada <a.garciap.2022@alumnos.urjc.es>
2024-07-17 13:31:09 +0200

    AstralBall

---
+++ Spells/AstralBall/AstralBall.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	pass # Replace with function body.
