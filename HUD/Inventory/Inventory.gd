69ed8d6b27c481d869ea5a5accf37e77a33348be
AlexGarciaPrada <a.garciap.2022@alumnos.urjc.es>
2024-07-20 16:13:58 +0200

    Ya se equipan los hechizos

--- HUD/Inventory/Inventory.gd
+++ HUD/Inventory/Inventory.gd
@@ -6,7 +6,7 @@ extends Control
 @onready var dark_grid = $Dark
 var show_element: GridContainer
 var show_num : int
-
+var num_pressed  = ""
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_show_square_cursor(fire_grid,0)
@@ -16,18 +16,24 @@ func _ready():
 func _process(delta):
 	if Singleton.is_stopped:
 		_cursor_movement()
+		if _get_element_square(show_element,show_num).this_texture.texture != null:
+			_create_num_input()
+			if num_pressed != "":
+				print(num_pressed)
+				num_pressed = _equip_spell(num_pressed)
+				print(num_pressed)
 	
 func _get_element_square (element: GridContainer, num:int):
-	return element.get_child(num)
+	return element.get_child(num).get_child(0)
 
 func _show_square_cursor(element: GridContainer, num:int):
-	var icon = _get_element_square(element,num).get_child(0)
+	var icon = _get_element_square(element,num)
 	show_element = element
 	show_num = num
 	icon._show_cursor()
 	
 func _hide_square_cursor(element: GridContainer, num:int):
-	var icon = _get_element_square(element,num).get_child(0)
+	var icon = _get_element_square(element,num)
 	icon._hide_cursor()
 
 func _join_elements_right(element:GridContainer):
@@ -77,3 +83,33 @@ func _cursor_movement():
 			_show_square_cursor(show_element,show_num - 2)
 		else:
 			_show_square_cursor(show_element,show_num + 4)
+
+func _create_num_input():
+	if Input.is_action_just_pressed("0"):
+		num_pressed = "0"
+	if Input.is_action_just_pressed("1"):
+		num_pressed ="1"
+	if Input.is_action_just_pressed("2"):
+		num_pressed = "2"
+	if Input.is_action_just_pressed("3"):
+		num_pressed = "3"
+	if Input.is_action_just_pressed("4"):
+		num_pressed = "4"
+	if Input.is_action_just_pressed("5"):
+		num_pressed = "5"
+	if Input.is_action_just_pressed("6"):
+		num_pressed = "6"
+	if Input.is_action_just_pressed("7"):
+		num_pressed = "7"
+	if Input.is_action_just_pressed("8"):
+		num_pressed = "8"
+	if Input.is_action_just_pressed("9"):
+		num_pressed = "9"
+
+func _equip_spell(input:String):
+	var square = get_parent()._get_spell_square(int(input))
+	print(int(input)+1)
+	print(square)
+	square.this_texture.texture = _get_element_square(show_element,show_num).this_texture.texture
+	square._make_texture()
+	return ""
