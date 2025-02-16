24f6bcd80cd0183e188758a3b523b3097fad30bd
AlexGarciaPrada <a.garciap.2022@alumnos.urjc.es>
2025-02-16 20:40:51 +0100

    little fix

--- door.gd
+++ door.gd
@@ -11,7 +11,7 @@ func _process(delta):
 	if !areas.is_empty():
 		for area in areas:
 			if area.get_parent().is_in_group("Player"):
-				Singleton.hud = area.get_parent().hud.duplicate()
+				#Singleton.hud = area.get_parent().hud.duplicate()
 				get_tree().root.add_child(scene)
 				get_tree().current_scene.free()
 				
