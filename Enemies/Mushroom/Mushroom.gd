2395a21cf888d3b3b98c46c0f07e65c2f844d828
Alejandro García Prada <114813960+AlexGarciaPrada@users.noreply.github.com>
2026-03-31 16:43:03 +0200

    mushroom fix

--- Enemies/Mushroom/Mushroom.gd
+++ Enemies/Mushroom/Mushroom.gd
@@ -8,19 +8,22 @@ var time = cooldown
 
 
 func _physics_process(delta):
-	_movement()
+	super._physics_process(delta)
 	_check_create_venom_cloud()
-
+	time+= delta
+	
 func _create_venom_cloud():
 	var venom_cloud = venom_cloud_scene.instantiate()
 	venom_cloud.global_position = self.global_position
 	get_parent().add_child(venom_cloud)
+	time = 0
 	
 func _check_create_venom_cloud():
 	if time >= cooldown && position.distance_to(player.position) < cloud_range:
 		_create_venom_cloud()
+		
 func is_player_in_area():
-	for area in enemyarea.get_overlapping_areas():
+	for area in enemy_area.get_overlapping_areas():
 		if area.get_parent().is_in_group("Player"):
 			return true
 	return false
